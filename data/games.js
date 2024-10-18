5d95a2b10d47ac0a1d7532886f421a2bfbf55fea
todoslosviernes625 <88558544+todoslosviernes625@users.noreply.github.com>
2024-10-18 13:11:46 -0400

    primera cersion del 1311 de octubre

---
+++ data/games.js
@@ -0,0 +1,52 @@
+var games = [
+  {
+    slug: 'cc1-primero',
+    name: 'CC. Paseo La Granja',
+    author: '15 (96 local)',
+    twitter: 'ccpaseolagranja',
+    website: 'tlv/primero',
+    github: 'github.com/bartaz/lost-in-cyberspace',
+  },
+  {
+    slug: 'cc2-segundo',
+    name: 'CRISTAL',
+    author: 'Ciudad Cristal',
+    twitter: 'centrocomercialcristal',
+    website: 'github.com/Platane',
+    github: 'github.com/Platane/js13k-2017',
+  },
+  {
+    slug: 'cc3-tercero',
+    name: 'La Granja',
+    author: 'La Granja',
+    twitter: 'captainpainway',
+    website: 'maryknize.com',
+    github: 'github.com/captainpainway/coconutty',
+  },
+  {
+    slug: 'cc4-cuarto',
+    name: 'Lost Pacman',
+    author: 'MarcGuinea',
+    twitter: 'MarcGuineaCasas',
+    website: 'marcguinea.com',
+    github: 'github.com/mguinea/lost-pacman',
+  },
+  {
+    slug: 'cc5-quinto',
+    name: 'Polyhedron Runner',
+    author: 'Alex Swan',
+    twitter: 'BoldBigflank',
+    website: 'bold-it.com',
+    github: 'github.com/BoldBigflank/js13k-polyhedron',
+  },
+  {
+    slug: 'cc6-sexto',
+    name: 'She is my universe',
+    author: 'Madmarcel',
+    twitter: 'madmarcel',
+    website: '',
+    github: 'github.com/madmarcel/js13k2017',
+  },
+
+
+];
